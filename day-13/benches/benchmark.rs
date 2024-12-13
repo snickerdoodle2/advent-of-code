98a95db2c9cfb4782ec535532e378d1cc9cf15fa
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-13 12:45:44 +0100

    feat: updated benchmark

--- day-13/benches/benchmark.rs
+++ day-13/benches/benchmark.rs
@@ -9,7 +9,9 @@ fn part1_bench() {
     part1::process(divan::black_box(include_str!("../input1.txt")));
 }
 
+const ERROR: u64 = 10_000_000_000_000;
+
 #[divan::bench]
 fn part2_bench() {
-    part2::process(divan::black_box(include_str!("../input2.txt")));
+    part2::process(divan::black_box(include_str!("../input2.txt")), ERROR);
 }
